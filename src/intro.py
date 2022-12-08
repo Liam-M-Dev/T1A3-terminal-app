4e3df880e697e1a60cba65cb0ec19a5a86a5781c
Liam-M-Dev <liammassey21@gmail.com>
2022-12-08 15:01:08 +1100

    Initilize bash script, create intro function

---
+++ src/intro.py
@@ -0,0 +1,26 @@
+
+# Introduction to haiku creator
+
+# Function to print out introduction and basic rules
+
+def intro_printout():
+    intro_text = """
+    Hello and welcome to haiku creator
+    This terminal app allows you to create your own haiku poems.
+    
+    So what is a haiku you ask?
+    A haiku is a small poem consisting of 3 lines and 17 syllables,
+    depicting a moment of insight followed by a moment of observation.
+
+    You can create your haiku in the create a haiku option.
+    The logic will then determine if it follows the basic rule of:
+    5 syllables in the first and third line, 7 syllables in the second.
+
+    You can also view and edit your files in the saved file option.
+    For extra fun, try the jumble option to jumble your haiku's in
+    the selected file!
+    """
+
+    return intro_text
+
+print(intro_printout())
--- src/main.py
+++ src/main.py
@@ -1,5 +1,6 @@
 from os import system
 from menu import main_menu
+from intro import intro_printout
 
 
 menu_option = ""
@@ -7,14 +8,25 @@ menu_option = ""
 while menu_option != "5":
     system("clear")
     menu_option = main_menu()
+    system("clear")
     if menu_option == "1":
-        print("This is the intro page")
+        print(intro_printout())
+        input("Press Enter to return:")
     elif menu_option == "2":
         print("Create haiku page")
+        input("Press Enter to return:")
     elif menu_option == "3":
         print("Saved files")
+        input("Press Enter to return:")
     elif menu_option == "4":
         print("jumbler")
-    else:
+        input("Press Enter to return:")
+    elif menu_option == "5":
         print("Exits program!")
+        input("Press Enter to return:")
+    else:
+        print("Sorry not a valid input, please select from the options")
+        input("Press Enter to return:")
+        
+
     
