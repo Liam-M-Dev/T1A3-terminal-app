9d96e26666f733e10044c8c36b790b97b5c200f5
Liam-M-Dev <liammassey21@gmail.com>
2022-12-09 18:48:42 +1100

    Implement haiku function for title and first line

--- src/haiku_poem.py
+++ src/haiku_poem.py
@@ -1,5 +1,34 @@
 import syllapy
 
 def haiku_creator():
-    title = input("Enter a title for your poem: ")
-    return title
+    poem = {}
+    # Get user input to create title of poem
+    poem_title = input("Enter a title for your poem: ")
+    poem["title"] = poem_title
+    # Get user input for each line of poem
+    
+    poem["line_one"] = five_syllable_line()
+
+        
+    
+        
+    # Check each line for syllable requirements
+    # Appened lines to empty dictionary
+    # return dictionary
+    print(poem)
+    return poem
+
+def syllable_counter(line):
+    count = syllapy.count(line)
+    return count
+
+def five_syllable_line():
+    while True:
+        user_input = input("Enter line: ")
+        line_count = syllable_counter(user_input)
+        if line_count < 5 or line_count > 5:
+            print("Incorrect amount of syllables, try again")
+        else:
+            return user_input
+
+haiku_creator()
