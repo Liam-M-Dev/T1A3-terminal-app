41bcbb06a8ebc97c9ba7e0f4f5ade6390a6219ec
Liam-M-Dev <liammassey21@gmail.com>
2022-12-13 15:28:07 +1100

    Start implementing jumbler feature

---
+++ src/jumbler.py
@@ -0,0 +1,23 @@
+from random import shuffle
+import json
+
+
+# Take user input to open up a selected file to randomize
+# check file selected has more than one poem
+def file_size(file_path):
+    with open(file_path, 'r') as file:
+        data = json.load(file)
+        if len(data) >= 2:
+            return data
+        else:
+            print("Unfortunately this file doesn't have enough poems")
+
+# randomize the file with shuffle
+def randomiser(poem_data):
+    poem_dictionary = zip(poem_data)
+    poem_dictionary = dict(poem_dictionary)
+    print(poem_dictionary.keys())
+# return the mess of a file to the viewer to see
+# ask if they would like to save file or return to the menu
+
+randomiser(file_size("./saved_files/sample_poems.json"))
